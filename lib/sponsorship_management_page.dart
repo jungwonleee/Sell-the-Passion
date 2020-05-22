a07271e407684f5f16cdd5978b85e15d20dd7f1f
Jungwon Lee <allnewjw@naver.com>
2020-05-22 13:16:04 +0900

    후원목표명 출력 성공

--- lib/sponsorship_management_page.dart
+++ lib/sponsorship_management_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:sell_the_passion/firebase_provider.dart';
+import 'package:provider/provider.dart';
 
 class SponsorshipManagementPage extends StatefulWidget {
   @override
@@ -6,16 +9,46 @@ class SponsorshipManagementPage extends StatefulWidget {
 }
 
 class _SponsorshipManagementPageState extends State<SponsorshipManagementPage> {
+  String slave, title;
+
   @override
   Widget build(BuildContext context) {
+    FirebaseProvider fp = Provider.of<FirebaseProvider>(context);
+    DatabaseReference tmpRef = FirebaseDatabase.instance.reference().child('${fp.getUser().uid}').child("slave");
+    
+    tmpRef.once().then((DataSnapshot snapshot) {
+      slave = snapshot.value as String; 
+    }).then((value) {
+      print(slave);
+      DatabaseReference dbRef = FirebaseDatabase.instance.reference().child('$slave').child("goal");
+      dbRef.once().then((DataSnapshot snapshot){
+        Map<dynamic, dynamic> map = snapshot.value as Map;
+        title = map["title"];
+        setState(() {});
+      });
+    });
+
+    
+
+    //Color mint = Theme.of(context).primaryColor;
+    //Color brown = Theme.of(context).accentColor;
+
+    
+
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('후원할 목표가 아직 없습니다.', style: TextStyle(fontSize: 20)),
-          Text('후원 매칭을 시작해보세요!', style: TextStyle(fontSize: 20))
-        ],
-      ),
+      child: Text('$title', style: TextStyle(fontSize: 20)),
     );
   }
 }
+
+/*
+return Center(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text('후원할 목표가 아직 없습니다.', style: TextStyle(fontSize: 20)),
+      Text('후원 매칭을 시작해보세요!', style: TextStyle(fontSize: 20))
+    ],
+  ),
+);
+*/
