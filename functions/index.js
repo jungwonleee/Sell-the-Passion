140a7738bb5068d6779a677ab1d444752f09b9fa
cdltlehf <cdltlehf@naver.com>
2020-06-05 19:41:32 +0900

    Add demo function

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,21 @@ admin.initializeApp();
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
 exports.addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
   const original = req.query.text;
-  admin.database().ref().once().
-  const snapshot = await admin.database().ref('/messages').push({original: original})
-  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  res.redirect(303, snapshot.ref.toString());
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin.database().ref('/ready_queue').once('value', (snapshot) => {
+    for (let category in snapshot.val()) {
+      for (let period in snapshot.val()[category]) {
+        const list = [];
+        for (let key in snapshot.val()[category][period])
+          list.push(snapshot.val()[category][period][key]);
+        for (let i=0;i<list.length-1;i++) {
+          admin.database().ref('/users').child(list[i]).update({ "slave": list[i+1], "user_state": 3 });
+        }
+        admin.database().ref('/users').child(list[list.length-1]).update({ "slave": list[0], "user_state": 3 });
+      }
+    }
+    res.json(snapshot.val());
+  });
 });
